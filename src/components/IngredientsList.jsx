edafc993990afaf72a4b98f92351d1fd8c78ab8e
Abraham Flynn <flynnabe@gmail.com>
2025-01-02 02:09:49 -0500

    changed to more than 0 problems

--- src/components/IngredientsList.jsx
+++ src/components/IngredientsList.jsx
@@ -6,7 +6,7 @@ export default function IngredientsList(props) {
         <section>
             <h2>Problems Inputted:</h2>
             <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
-            {props.ingredients.length > 2 && <div className="get-recipe-container">
+            {props.ingredients.length > 0 && <div className="get-recipe-container">
                 <div>
                     <h3>Ready For A Techy Fix</h3>
                     <p>Get a technology diagnosis and fix in seconds</p>
